020640297fffaa85f728180585aaa7230d282174
Vinay Pratap Karwal <vinaypratapkarwal@gmail.com>
2024-12-16 05:38:16 +0530

    Added Color Schema to the codebase

---
+++ lib/common/widgets/colors/color.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFFFFFFFF);
+  static const Color primary = Color(0xFF1F9686);
+  static const Color secondary = Color(0xFF211F96);
+  static const Color accent = Color(0xFF212121);
+  static const Color text = Color(0xFF000000);
+  static const Color success = Color(0xFF36B23B);
+  static const Color error = Color(0xFFD90000);
+  static const Color subText = Color(0xFF808080);
+  static const Color stroke = Color(0xFFB3B3B3);
+}
